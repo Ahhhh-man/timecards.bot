66b2af658a6a6eb5460375b54c2edbcce655899f
Aman <Aman.Uddin@homeoffice.gov.uk>
2022-06-04 01:10:23 +0100

    updated commands

--- cogs/custom.py
+++ cogs/custom.py
@@ -34,6 +34,38 @@ class Custom(commands.Cog, name='Platform Commands'):
     async def sink(self, ctx):
         await ctx.send(os.environ.get('CUSTOM_IMAGE_URL_1'))
 
+    
+    @commands.command(name="clown")
+    @commands.guild_only()
+    async def clown(self, ctx, agent: str = None):
+        CLOWN_URL = os.environ.get('CUSTOM_IMAGE_URL_2')
+        clowns = {
+            'breach': "/894897773575557140/breach.png",
+            'cypher': "/894897784325546055/cypher.png",
+            'omen': "/894897796317061170/omen.png",
+            'jett': "/894897808941920306/jett.png",
+            'kj': "/894897816588148736/kj.png",
+            'killjoy': "/894897816588148736/kj.png",
+            'phoenix': "/894897824112717834/phoenix.png",
+            'raze': "/894897827354935296/raze.png",
+            'sage': "/894897828911005706/sage.png",
+            'viper': "/894897830093791272/viper.png",
+            'reyna': "/894897830152523776/reyna.png",
+            'skye': "/894897834216796190/skye.png",
+            'yoru': "/894897834753675284/yoru.png"
+        }
+
+        # if the user doesnt provide an agent, then we will use a random agent
+        if not agent:
+            await ctx.send(CLOWN_URL + random.choice(list(clowns.values())))
+        else:
+        # check if the input is a valid agent
+            try:
+                agent_url = clowns[agent]
+                await ctx.send(CLOWN_URL + agent_url)
+            except KeyError:
+                await ctx.send("You're the clown!")
+
 
 async def setup(bot):
     await bot.add_cog(Custom(bot))
